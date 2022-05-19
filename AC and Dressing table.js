84f3b3e9654b944879a81094dbac5b9adb003593
AdityaT1610 <84332161+AdityaT1610@users.noreply.github.com>
2022-05-19 16:01:38 +0530

    Add files via upload

---
+++ AC and Dressing table.js
@@ -0,0 +1,28 @@
+img ="";
+status="";
+
+function preload() 
+{
+    img = loadImage('ADT.jpg');
+}
+
+function setup() 
+{
+    canvas = createCanvas(650, 450);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status - detecting objects";
+
+}
+
+function modelLoaded()
+{
+    console.log("Model is Loaded");
+    status = true;
+    objectDetector.detect(img, gotRsults);
+}
+
+function back()
+{
+    window.location = "index.html"
+}
